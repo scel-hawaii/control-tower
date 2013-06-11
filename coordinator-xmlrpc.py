c213ba34fdae4edf8d462bab4f34bf784de75516
Adam Oberbeck <asoberbeck@yahoo.com>
2013-06-10 17:02:46 -1000

    server: *that* table name changed a while ago...!

--- coordinator-xmlrpc.py
+++ coordinator-xmlrpc.py
@@ -22,7 +22,7 @@ def last_reading_by_node():
     c = db_conn.cursor()
     # subtlety: cast time to string only for results, not every row:
     # fixme: seq scan
-    c.execute("select raaargh.address, db_time::varchar, bmp085_temp_decic, bmp085_press_pa, batt_mv, panel_mv from raaargh, (select address, max(db_time) from raaargh group by address) a where raaargh.address = a.address and raaargh.db_time = a.max;")
+    c.execute("select outdoor_env.address, db_time::varchar, bmp085_temp_decic, bmp085_press_pa, batt_mv, panel_mv from outdoor_env, (select address, max(db_time) from outdoor_env group by address) a where outdoor_env.address = a.address and outdoor_env.db_time = a.max;")
     l = [["address", "db_time", "bmp085_temp_decic", "bmp085_press_pa", "batt_mv", "panel_mv"]]
     l.extend(list(c))
     c.close()
@@ -30,7 +30,7 @@ def last_reading_by_node():
 
 def last_hour_avg_by_node():
     c = db_conn.cursor()
-    c.execute("select address, min(db_time)::varchar as min_db_time, max(db_time)::varchar as max_db_time, count(*) as n, avg(bmp085_temp_decic)::double precision as avg_bmp085_temp_decic, avg(bmp085_press_pa)::double precision as avg_press_pa from raaargh where db_time > now() - '1 hour'::interval group by address;")
+    c.execute("select address, min(db_time)::varchar as min_db_time, max(db_time)::varchar as max_db_time, count(*) as n, avg(bmp085_temp_decic)::double precision as avg_bmp085_temp_decic, avg(bmp085_press_pa)::double precision as avg_press_pa from outdoor_env where db_time > now() - '1 hour'::interval group by address;")
     l = [["address", "min_db_time", "max_db_time", "n", "avg_bmp085_temp_decic", "avg_pressure_pa", "avg_batt_mv", "avg_panel_mv"]]
     l.extend(list(c))
     c.close()
