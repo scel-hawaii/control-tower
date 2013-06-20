512ac535f3c8580e2ee1f903f5ec738a28c28636
Adam Oberbeck <asoberbeck@yahoo.com>
2013-06-19 17:58:35 -1000

    server: add decoder library supporting multiple packet formats

--- coordinator-recv-db.py
+++ coordinator-recv-db.py
@@ -7,6 +7,8 @@ import json
 import sys
 import time
 
+import decode
+
 time_fmt = '%FT%T %z'
 
 try:
@@ -18,37 +20,30 @@ xbee = ZigBee(ser, escaped=True)
 conn = psycopg2.connect(host="localhost", port="5432", user="sensornet", password="easyp4ss!", database="sensing_monitoring")
 cur = conn.cursor()
 
-insert_query = """begin; insert into outdoor_env (address, db_time, uptime_ms, bmp085_temp_decic, bmp085_press_pa, batt_mv, panel_mv, apogee_mv, apogee_w_m2) values (%(address)s, now(), %(uptime_ms)s, %(bmp085_temp_decic)s, %(bmp085_press_pa)s, %(batt_mv)s, %(panel_mv)s, %(apogee_mv)s, %(apogee_w_m2)s); commit;"""
-
 print time.strftime(time_fmt),
 print "starting..."
 
 while True:
     try:
-        values_str = xbee.wait_read_frame()['rf_data']
+        rf_data = xbee.wait_read_frame()['rf_data']
 
         try:
-            values = json.loads(values_str)
-        except ValueError, e:   # can't decode JSON -- log it!
-            cur.execute("""begin; insert into outdoor_env_unrecognized (db_time, rf_data, exception) values (now(), %s, %s); commit;""",
-                        [buffer(values_str), str(e)])
+            cur.execute('BEGIN;')
+            for t in decode.decode(rf_data):
+                cur.execute(decode.create_query(t), t['values'])
+            cur.execute('COMMIT;')
+        except Exception, e:
+            cur.execute('ROLLBACK;')
+            cur.execute('BEGIN;')
+            cur.execute(
+                '''INSERT INTO outdoor_env_unrecognized (db_time, rf_data, exception) VALUES (now(), %s, %s);''',
+                [buffer(rf_data), str(e)])
+            cur.execute('COMMIT;')
             continue
 
-        print time.strftime(time_fmt),
-        print values_str
-        sys.stdout.flush()
-
-        for field in ['apogee_mv', 'apogee_w_m2', 'address', 
-                      'uptime_ms', 'bmp085_temp_decic', 
-                      'bmp085_press_pa', 'batt_mv', 'panel_mv']:
-            values.setdefault(field, None)
-        if values.has_key('addr') and values['addr'] == 13:
-            cur.execute("""begin; insert into greenbox (addr, db_time, up_ms, bmp_c, bmp_pa, sht_pct, ds20_c, tsl_m2, solar_v, shunt_mv, solar_ma, batt_v, batt_mv, batt_ma) values (%(addr)s, now(), %(up_ms)s, %(bmp_c)s, %(bmp_pa)s, %(sht_pct)s, %(ds20_c)s, %(tsl_m2)s, %(solar_v)s, %(shunt_mv)s, %(solar_ma)s, %(batt_v)s, %(batt_mv)s, %(batt_ma)s); commit;""", values)
-            continue
-        cur.execute(insert_query, values)
     except:
         print time.strftime(time_fmt),
-        print "ser.close()"
+        print "stopping!"
         ser.close()
         cur.close()
         conn.close()
---
+++ decode.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+import json
+import struct
+
+def decode_json(s):
+    return [{'time_offset_s': 0,
+             'values': json.loads(s)}]
+
+def unpack_1(s):
+    struct_fmt = '<HHIB' + 'H'*15 + 'H'*15 + 'IhH' + 'h'*60 + 'BBBBBBBiii'
+    values_list = struct.unpack(struct_fmt, s)
+    values = {}
+    for key, start, end in [('schema', 0, 1),
+                            ('address', 1, 2),
+                            ('uptime_ms', 2, 3),
+                            ('n', 3, 4),
+                            ('batt_mv', 4, 19),
+                            ('panel_mv', 19, 34),
+                            ('bmp085_press_pa', 34, 35),
+                            ('bmp085_temp_decic', 35, 36),
+                            ('humidity_centi_pct', 36, 37),
+                            ('apogee_w_m2', 37, 97),
+                            ('gps_valid_sats', 97, 98),
+                            ('gps_year', 98, 99),
+                            ('gps_month', 99, 100),
+                            ('gps_day', 100, 101),
+                            ('gps_hour', 101, 102),
+                            ('gps_minute', 102, 103),
+                            ('gps_second', 103, 104),
+                            ('gps_lat', 104, 105),
+                            ('gps_lon', 105, 106),
+                            ('gps_elev', 106, 107)
+                            ]:
+        values[key] = values_list[start:end]
+        if len(values[key]) == 1:
+            values[key] = values[key][0]
+    return values
+
+def decode_1(s):
+    p = unpack_1(s)
+
+    time_series = []
+
+    n = p['n']            # number of data points in this packet 1..60
+
+    for i in xrange(n-1, -1, -1):
+        query_values = {'address': p['address']}
+
+        # values included in the *last* sample of a packet
+        if i == n-1:
+            query_values['uptime_ms'] = p['uptime_ms']
+            query_values['bmp085_press_pa'] = p['bmp085_press_pa']
+            query_values['bmp085_temp_decic'] = p['bmp085_temp_decic']
+            query_values['humidity_centi_pct'] = p['humidity_centi_pct']
+
+        # subtract n - i - 1 from now() to get this datum's db_time
+        # offset that will be used in the insertion query
+        time_offset_s = -(n - i - 1)
+
+        # values included in *every* sample of a packet
+        query_values['apogee_w_m2'] = p['apogee_w_m2'][i]
+
+        # values included every couple samples of a packet
+        if (i+1) % 4 == 0 or i == n-1:
+            query_values['batt_mv'] = p['batt_mv'][i/4]
+            query_values['panel_mv'] = p['panel_mv'][i/4]
+
+        time_series.append({'time_offset_s': time_offset_s,
+                        'values': query_values})
+
+    return time_series
+
+def create_query(v):
+    columns = sorted(v['values'].keys())
+    column_names = ', '.join(columns)
+    placeholders = ', '.join(['%(' + x + ')s' for x in columns])
+    return '''INSERT INTO outdoor_env (db_time, %s) VALUES (now() + '%s seconds'::interval, %s)''' % (column_names, v['time_offset_s'], placeholders)
+
+decoders = {
+    8827: decode_json,          # '{"' -> 8827
+    1: decode_1
+    }
+
+def decode(rf_data):
+    schema = struct.unpack('<H', rf_data[0:2])[0]
+    return decoders[schema](rf_data)
