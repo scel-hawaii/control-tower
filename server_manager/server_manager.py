f94fb0d7ba7dab668d6a0d59fef0d50daa626777
Kenny Luong <kenny@debian>
2013-10-20 20:37:40 -1000

    Screen checking doesn't work ATM

--- server_manager/server_manager.py
+++ server_manager/server_manager.py
@@ -11,13 +11,21 @@ class ScreenSupervisor:
 		command = ['screen', '-d', '-m', '-S', session_name]
 		subprocess.check_output(command)
 	
+	
 	def exists_session(self, session_name):
 		command = ['screen', '-ls', '|', 'grep', session_name]
+		print "Looking for " + session_name
+		output = subprocess.check_call(command)
 		try: 
-			subprocess.check_output(command)
-			return True
+			print "Here is the output: " + output
+			if output > 0:
+				return True
+			else:
+				return False
 		except:
+			print "Error"
 			return False
+
 
 	def kill_session(self, session_name):
 		if exists_session(session_name):
@@ -26,7 +34,7 @@ class ScreenSupervisor:
 			print "The screen doesn't exist. We can't kill it"
 
 
-class REISManager:
+class ControlTower:
 	SERVER_FILE = "while.py"
 	SCREEN_NAME = 'TesterScreen'
 
@@ -34,29 +42,29 @@ class REISManager:
 		self.print_start_message()
 		self.screen_super = ScreenSupervisor()
 
-		self.start_coordinator_server()
+		#self.start_server()
 
-	def start_coordinator_server(self):
-		self.start_coordinator_screen()
-		self.start_coordinator_process()
+	def start_server(self):
+		self.start_screen()
+		self.start_process()
 
 
-	def start_coordinator_screen(self):
-		if self.screen_super.exists_session(REISManager.SCREEN_NAME):
+	def start_screen(self):
+		if self.screen_super.exists_session(ControlTower.SCREEN_NAME):
 			print "The screen already exists."
 		else:
-			self.screen_super.start_session(REISManager.SCREEN_NAME)
+			self.screen_super.start_session(ControlTower.SCREEN_NAME)
 
-	def start_coordinator_process(self):
-		if (self.server_process_exists()):
+	def start_process(self):
+		if (self.exists_process()):
 			print "Process Already exists"
 		else:
 			print "Lets go ahead and start the process"
-			command = ['screen', '-S', REISManager.SCREEN_NAME, '-X', 'stuff', 'python while.py\n']
+			command = ['screen', '-S', ControlTower.SCREEN_NAME, '-X', 'stuff', 'python while.py\n']
 			subprocess.call(command)
 		
-	def kill_coord_server_process(self):
-		PID = int(self.check_status_server())
+	def kill_process(self):
+		PID = int(self.check_status_process())
 		print "PID " + str(PID)
 
 		PID_STR = str(PID)
@@ -69,7 +77,7 @@ class REISManager:
 		else:
 			print "The server process isn't running! We can't kill it!"
 
-	def check_status_server(self):
+	def check_status_process(self):
 		check_command = ["pgrep", "-f", "while.py"]
 		try:
 			output = subprocess.check_output(check_command)
@@ -78,19 +86,25 @@ class REISManager:
 			print "Server process is NOT running!"
 			return -1
 
-	def server_process_exists(self):
-		PID = self.check_status_server()
+	def exists_process(self):
+		PID = self.check_status_process()
 		if PID != -1:
 			return True
 		else:
 			return False
 
-	def print_status_server(self):
-		PID = self.check_status_server()
+	def print_status_process(self):
+		PID = self.check_status_process()
 		if PID != -1:
 			print "The coordinator server process is running on PID: " + str(PID)
 		else:
 			print "The coordinator server process is NOT running."
+
+	def print_status_screen(self):
+		if self.screen_super.exists_session(ControlTower.SCREEN_NAME):
+			print "The screen " + ControlTower.SCREEN_NAME + " exists."
+		else:
+			print "The screen does not exist"
 
 
 
@@ -116,6 +130,8 @@ class REISManager:
 		x = x + "(start-process) - Start coordinator process\n"
 		x = x + "(menu) - List Menu Options\n"
 		x = x + "(status) - Check server status\n"
+		x = x + "(check-screen) - Check screen status\n"
+		x = x + "(check-process) - Check server status\n"
 		x = x + "(kill-server) - Kill server\n"
 		x = x + "(quit) - Quit Program\n"
 		x = x + "===============\n"
@@ -127,22 +143,26 @@ class REISManager:
 
 	def execute_menu_option(self, input):
 		if input == "start":
-			self.start_coordinator_server()
+			self.start_server()
 		elif input == "start-process":
-			self.start_coordinator_process()
+			self.start_process()
 		elif input == "menu":
 			print self.print_menu()
 		elif input == "status":
-			self.print_status_server()
+			self.print_status_process()
+		elif input == "check-screen":
+			self.print_status_screen()
+		elif input == "check-process":
+			self.print_status_process()
 		elif input == "kill-server":
-			self.kill_coord_server_process()
+			self.kill_process()
 		elif input == "quit":
 			exit()
 		else:
 			print "Invalid menu command. Please try again."
 
 if __name__ == "__main__":
-	supervisor = REISManager()
+	supervisor = ControlTower()
 	print supervisor.print_menu()
 	while True:
 		supervisor.get_menu_option()
