207fc72b22667a64bfd35581f6510dde6b0c7d02
Kenny Luong <luong97@hawaii.edu>
2014-04-01 09:00:10 -1000

    Added debug statements

--- utils/db_csv_website/sync.rb
+++ utils/db_csv_website/sync.rb
@@ -7,27 +7,33 @@ require 'pg'
 
 # Fetch the list of weatherbox addresses from the DB
 def get_weatherbox_addresses
+  puts "Getting weatherbox addresses..." 
   @res.each do |item|
     @box_addr.push(item['address'])
   end
+  puts "Finished getting weatherbox addresses." 
 end
 
 # Create the CSV files from the addresses 
 def create_csv_from_addresses(dir)
+  puts "Creating CSV files from address..."
   @box_addr.each do |addr|
     ## Export the entire raw database for each node id
     command = " psql -c '\\copy (SELECT * FROM outdoor_env WHERE (apogee_w_m2 IS NOT NULL AND address=#{addr}) ORDER BY db_time DESC) To #{dir}#{addr}-data.csv With CSV header\'"
     `#{command}`
   end
+  puts "Finished getting CSV files from addresses."
 end
 
 # Create 3-day CSVs from the DB
 def create_threeday_csv(dir)
+  puts "Creating threeday_csv files..."
   @box_addr.each do |addr|
     ## Same as above, but with a limit of 3 days. 
     command = " psql -c '\\copy (SELECT * FROM outdoor_env WHERE (apogee_w_m2 IS NOT NULL AND address=#{addr}) ORDER BY db_time DESC LIMIT 129600) To #{dir}#{addr}-data-threeday.csv With CSV header\'"
     `#{command}`
   end
+  puts "Finished creating threeday_csv files."
 end
 
 # Sync the files up..
