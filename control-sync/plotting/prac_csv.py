f50219a46d68db115b62511041bd03a14a804996
MasaharuKage <rwalser@hawaii.edu>
2014-06-25 15:32:51 -1000

    Reading a csv file practice

--- control-sync/plotting/prac_csv.py
+++ control-sync/plotting/prac_csv.py
@@ -1,9 +1,19 @@
 import csv
 import numpy
+import operator
 
 x = []
 
-with open('215_data.csv', 'r') as csvfile:
-	reader = csv.DictReader(csvfile)
-	for row in reader:
-		x.append(row)
+#with open('215_data.csv', 'r') as csvfile:
+#	reader = csv.DictReader(csvfile)
+#	for row in reader:
+#		x.append(row)
+
+data = open('215_data.csv', 'r')
+
+data1 = csv.reader(data, delimiter = '\n')
+
+for row in data1:
+	print(row)
+
+data.close()
