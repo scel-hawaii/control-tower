89d4076149455a28ab2b4e4c4202995639227fa4
MasaharuKage <rwalser@hawaii.edu>
2014-06-26 14:35:34 -1000

    Edited practice file, problem with 0 VS no data

--- control-sync/plotting/prac_csv.py
+++ control-sync/plotting/prac_csv.py
@@ -1,19 +1,36 @@
+#Import proper Libraries
 import csv
 import numpy
 import operator
 
+#Test function
+def convert_to_int(field):
+	row[field] = int(row[field])
+
+#Establish variables
 x = []
 
-#with open('215_data.csv', 'r') as csvfile:
-#	reader = csv.DictReader(csvfile)
-#	for row in reader:
-#		x.append(row)
-
+#Open Data File
 data = open('215_data.csv', 'r')
 
-data1 = csv.reader(data, delimiter = '\n')
+#Gather the Data
+data1 = csv.DictReader(data)
 
+#Examine data 
 for row in data1:
-	print(row)
+	#Create proper types
+	convert_to_int("address")
+	#convert_to_int("uptime_ms")
+	#convert_to_int("bmp085_temp_decic")
+	#convert_to_int("bmp085_press_pa")
+	#convert_to_int("batt_mv")
+	x.append(row)
 
+#Print the data
+print(x)
+
+#Number of fields from data
+print(len(x))
+
+#Close file
 data.close()
