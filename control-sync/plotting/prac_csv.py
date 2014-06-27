41d3964b6f8b933ab9dfb0ff00d07f374efb9c6d
MasaharuKage <rwalser@hawaii.edu>
2014-06-26 16:03:31 -1000

    Updated for instances of no data

--- control-sync/plotting/prac_csv.py
+++ control-sync/plotting/prac_csv.py
@@ -4,8 +4,13 @@ import numpy
 import operator
 
 #Test function
-def convert_to_int(field):
-	row[field] = int(row[field])
+def convert_to_float(field):
+	#Check if there is data
+	if(row[field] == ''):
+		#No data = nan
+		row[field] = float('nan')
+		return
+	row[field] = float(row[field])
 
 #Establish variables
 x = []
@@ -19,11 +24,19 @@ data1 = csv.DictReader(data)
 #Examine data 
 for row in data1:
 	#Create proper types
-	convert_to_int("address")
-	#convert_to_int("uptime_ms")
-	#convert_to_int("bmp085_temp_decic")
-	#convert_to_int("bmp085_press_pa")
-	#convert_to_int("batt_mv")
+	convert_to_float("address")
+	convert_to_float("uptime_ms")
+	convert_to_float("bmp085_temp_decic")
+	convert_to_float("bmp085_press_pa")
+	convert_to_float("batt_mv")
+	convert_to_float("panel_mv")
+	convert_to_float("apogee_mv")
+	convert_to_float("apogee_w_m2")
+	convert_to_float("dallas_amb_c")
+	convert_to_float("dallas_roof_c")
+	convert_to_float("panel_ua")
+	convert_to_float("humidity_centi_pct")
+	convert_to_float("indicator")
 	x.append(row)
 
 #Print the data
--- control-sync/plotting/prac_plot.py
+++ control-sync/plotting/prac_plot.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #Fields
-x = [1, 2, 3, 4, 9]
-y = [2, 4, 6, 8, 12]
-w = [1, 3, 6, 9]
-z = [1, 4, 8, 12]
+x = [1, 2, 3, 4, 9, 10]
+y = [2, 4, 6, 8, 9, 12]
+w = [1, 3, 6, 9, 10]
+z = [1, 3, float('nan'), 7, 8]
 
 #Practice Plot
 plt.plot(x,y)
