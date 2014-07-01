2061c96c2c7d6a3590376fc7811850923712b8d0
MasaharuKage <rwalser@hawaii.edu>
2014-06-30 16:30:47 -1000

    Identified more errors, added meaningful names

--- control-sync/plotting/prac_csv.py
+++ control-sync/plotting/prac_csv.py
@@ -13,16 +13,20 @@ def convert_to_float(field):
 	row[field] = float(row[field])
 
 #Establish variables
+data = []
 x = []
+y = []
+
 
 #Open Data File
-data = open('215_data.csv', 'r')
+datafile = open('215_data.csv', 'r')
 
 #Gather the Data
-data1 = csv.DictReader(data)
+dataorganized = csv.DictReader(datafile)
 
 #Examine data 
-for row in data1:
+for row in dataorganized:
+
 	#Create proper types
 	convert_to_float("address")
 	convert_to_float("uptime_ms")
@@ -37,13 +41,15 @@ for row in data1:
 	convert_to_float("panel_ua")
 	convert_to_float("humidity_centi_pct")
 	convert_to_float("indicator")
-	x.append(row)
+	data.append(row) #ERROR: Currently storing data randomly
+
+#Pull pull specific field values
 
 #Print the data
-print(x)
+print(data[0])
 
 #Number of fields from data
-print(len(x))
+print(len(data))
 
 #Close file
-data.close()
+datafile.close()
