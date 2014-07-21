612be827e54eef21485ddf96d4a6c51505218dba
Kenny Luong <luong97@hawaii.edu>
2014-07-20 17:30:45 -1000

    Added a basic frontend to status_report

---
+++ status_report/index.php
@@ -0,0 +1,15 @@
+<html>
+<head></head>
+<body>
+<pre>
+<?php
+// exec('ruby status_report.rb', $output);
+// foreach($output as $line){
+  //echo $line;
+// }
+echo system('ruby status_report.rb');
+?>
+
+</pre>
+</body>
+</html>
