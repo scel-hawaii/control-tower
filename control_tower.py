6c961259cf8f874158f4d03d8926a7a2b80624b6
Kenny Luong <luong97@hawaii.edu>
2014-07-27 19:22:08 -1000

    Added update script

--- control_tower.py
+++ control_tower.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python
 import sys
 import os
+import subprocess
 
 arguments = sys.argv
 option = sys.argv[1]
 
+
+def get_version():
+    command = "git describe --abbrev=0 --tags".split(" ")
+    output = subprocess.Popen(command, stdout=subprocess.PIPE).communicate()[0].rstrip()
+    return output
+
+def update():
+    print "Updating control_tower..."
+    version = get_version()
+    print "Current version %s" % version
+    command = "git checkout %s" % version
+    print "Running command: %s" % command
+    command = command.split(" ")
+    output = subprocess.Popen(command, stdout=subprocess.PIPE).communicate()[0].rstrip()
+    
 if len(arguments) > 1:
     if option == "start":
         from coordinator_recv.coordinator_server import ServerLauncher
@@ -12,12 +28,11 @@ if len(arguments) > 1:
         launcher.start_server()
     elif option == "status":
         print "Option not implemented yet"
-    elif option == "update":
-        print "Updating control_tower"
     elif option == "version":
-        os.system("git describe --abbrev=0 --tags")
+        print get_version()
+    elif option == "update":
+        update()
 
 else:
     print "Usage: control_tower [option]"
     print
-
