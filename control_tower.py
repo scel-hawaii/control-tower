af7f4af5edfa26dbefeee5214bbac59630b1b8fc
Kenny Luong <luong97@hawaii.edu>
2014-07-27 20:24:19 -1000

    Updated makefile

--- control_tower.py
+++ control_tower.py
@@ -12,14 +12,24 @@ def get_version():
     output = subprocess.Popen(command, stdout=subprocess.PIPE).communicate()[0].rstrip()
     return output
 
+
+def fetch():
+    print "Fetching latest version..."
+    command = "git fetch"
+    command = command.split(" ")
+    output = subprocess.Popen(command, stdout=subprocess.PIPE).communicate()[0].rstrip()
+    print output
+    
 def update():
     print "Updating control_tower..."
+    fetch()
     version = get_version()
     print "Current version %s" % version
     command = "git checkout %s" % version
     print "Running command: %s" % command
     command = command.split(" ")
     output = subprocess.Popen(command, stdout=subprocess.PIPE).communicate()[0].rstrip()
+    print output
     
 if len(arguments) > 1:
     if option == "start":
