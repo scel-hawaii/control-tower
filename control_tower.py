a93b879e88b0a5dd3c252feb9de8af36043152c3
Kenny Luong <luong97@hawaii.edu>
2014-07-27 18:04:34 -1000

    Added basic control_tower features

--- control_tower.py
+++ control_tower.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
+import sys
+import os
 
-from coordinator_recv.coordinator_server import ServerLauncher
-launcher = ServerLauncher()
-launcher.start_server()
+arguments = sys.argv
+option = sys.argv[1]
+
+if len(arguments) > 1:
+    if option == "start":
+        from coordinator_recv.coordinator_server import ServerLauncher
+        launcher = ServerLauncher()
+        launcher.start_server()
+    elif option == "status":
+        print "Option not implemented yet"
+    elif option == "update":
+        print "Updating control_tower"
+    elif option == "version":
+        os.system("git describe --abbrev=0 --tags")
+
+else:
+    print "Usage: control_tower [option]"
+    print
 
