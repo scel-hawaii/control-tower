97bed19928bdbf95c72bdf4769b2d2e4f4f53cb9
Kenny Luong <luong97@hawaii.edu>
2014-10-05 00:33:43 -1000

    Removed old scripts

--- vnc_scripts/start_vnc.sh
+++
@@ -1,8 +0,0 @@
-vncserver -geometry 1600x1200 -randr 1600x1200,1440x900,1024x768
-#Then resize with:
-#
-#xrandr -s 1600x1200
-#xrandr -s 1440x900
-#xrandr -s 1024x768
-
-
--- vnc_scripts/x11vnc_script.py
+++
@@ -1,6 +0,0 @@
-from subprocess import call
-import time
-while True:
-	command = ["x11vnc", "-auth", "/home/kenny/.Xauthority", "-display", ":0"]
-	call(command)
-	time.sleep(0.1)
