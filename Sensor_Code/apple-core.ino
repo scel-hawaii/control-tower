7ac09aabe65af8f1b52fa77c5bc013f479c454ea
MasaharuKage <rwalser@hawaii.edu>
2015-10-26 17:05:39 -1000

    File updates and clean up

--- Sensor_Code/apple-core.ino
+++ Sensor_Code/apple-core.ino
@@ -23,7 +23,32 @@ void setup(){
 }
 
 void loop(){
+    int BatterymV;
+    int SolarIrrmV;
+    int Humiditypct;
+    int PanelmV;
+    int Pressurepa;
+    int Tempdecic;
+    
+    BatterymV = Sensors_sampleBatterymV();
+    delay(1000);
+    SolarIrrmV = Sensors_sampleSolarIrrmV();
+    delay(1000);
+    Humiditypct = Sensors_sampleHumiditypct();
+    delay(1000);
+    PanelmV = Sensors_samplePanelmV();
+    delay(1000);
+    Tempdecic = Sensors_sampleTempdecic();
+    delay(1000);
+    Pressurepa = Sensors_samplePressurepa();
+    delay(1000);
 
-
+    Serial.print("Sample Data:\n");
+    Serial.println(BatterymV);
+    Serial.println(SolarIrrmV);
+    Serial.println(Humiditypct);
+    Serial.println(PanelmV);
+    Serial.println(Pressurepa);
+    Serial.println(Tempdecic);
 
 }
