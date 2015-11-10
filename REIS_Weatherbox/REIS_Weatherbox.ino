a042d6def9b1929062d75788a69a5f952014e92e
MasaharuKage <rwalser@hawaii.edu>
2015-11-09 16:56:22 -1000

    Updates to Test Main files and Main File

---
+++ REIS_Weatherbox/REIS_Weatherbox.ino
@@ -0,0 +1,56 @@
+/*********************************************
+ *
+ *    File: REIS_Weatherbox.ino
+ *    REIS Weatherbox Firmware
+ *
+ *    The main driver file for the REIS Weatherbox
+ *
+ ********************************************/
+
+/* Program Libraries */
+#include "config.h"
+#include "sensors.h"
+#include "transmit.h"
+
+/*********************************************
+ *
+ *    Name:        setup
+ *    Returns:     Nothing
+ *    Parameter:   Nothing
+ *    Description: Runs once at the start of the sequence.
+ *                     Used for initialization. Place all
+ *                     init scripts here.
+ * 
+ ********************************************/
+void setup(){
+
+    /* Generation Check */
+
+    /* Initialization */
+
+    /* Health Check */
+
+}
+
+/*********************************************
+ *
+ *    Name:        loop
+ *    Returns:     Nothing
+ *    Parameter:   Nothing
+ *    Description: Main program function, runs constantly.
+ *                     Executed after setup(), and will
+ *                     continue running indefinitely,
+ *                     unless conditions stop it.
+ * 
+ ********************************************/
+void loop(){
+
+    /* Poll Sensors */
+
+    /* Packet Construction */
+
+    /* Transmit Packet */
+
+    /* Clear Packet Buffer */
+
+}
