006a90e2f27b35ae69f5f9273249123f5730ff7f
Musicality <scottbn@hawaii.edu>
2015-11-18 15:35:08 -1000

    Updated utlities.h

--- REIS_Weatherbox/utilities.h
+++ REIS_Weatherbox/utilities.h
@@ -12,4 +12,6 @@
 #define FALSE 0
 #define ADC_SAMPLE_NUM 200
 
-
+int chk_overflow(unsigned long current_value, unsigned long previous_value);
+long sampleBatteryVoltage(void);
+void configureADC(void);
