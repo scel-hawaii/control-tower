e5f5bde0e266ccfabe482f4bb66d74c97f031111
Kenny Luong <luong97@hawaii.edu>
2016-03-26 21:03:22 -1000

    Added file logging to worker

--- coordinator_recv/worker.py
+++ coordinator_recv/worker.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import pika
 import pickle
+import logging
+
+logging.basicConfig(filename='worker.log',level=logging.DEBUG)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
         host='localhost'))
@@ -10,7 +13,9 @@ channel.queue_declare(queue='hello')
 
 def callback(ch, method, properties, body):
     d = pickle.loads(body)
-    print d['timestamp'], d['xbee_frame']['rf_data']
+
+    s = str(d['timestamp']) +  str(d['xbee_frame']['rf_data'])
+    logging.info(s)
 
 channel.basic_consume(callback,
                       queue='hello',
