07b43c5a41de4b3e07f6815fa59e0726fba8c54d
Kenny Luong <luong97@hawaii.edu>
2016-03-29 17:27:04 -1000

    Added worker to consume weather data

--- coordinator_recv/worker.py
+++ coordinator_recv/worker.py
@@ -1,25 +1,38 @@
 #!/usr/bin/env python
 import pika
-import pickle
+import cPickle as pickle
 import logging
+import simple_decoder
+import thread
+import datetime
+from decode import PacketDecoder
+
+
+decoder = PacketDecoder()
+i = 0;
 
 logging.basicConfig(filename='worker.log',level=logging.DEBUG)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
         host='localhost'))
 channel = connection.channel()
-
 channel.queue_declare(queue='hello')
 
-def callback(ch, method, properties, body):
+def parse_data(body):
     d = pickle.loads(body)
+    rf_data = d['xbee_frame']['rf_data']
+    decoder.decode(rf_data)
+    print "Finished decoding data"
 
-    s = str(d['timestamp']) +  str(d['xbee_frame']['rf_data'])
-    logging.info(s)
+def callback(ch, method, properties, body):
+    # thread.start_new_thread( parse, (body,))
+    parse_data(body)
 
-channel.basic_consume(callback,
-                      queue='hello',
-                      no_ack=True)
+def consume(channel):
+    channel.basic_consume(callback,
+                          queue='hello',
+                          no_ack=True)
+    while True:
+        channel.start_consuming()
 
-while True:
-    channel.start_consuming()
+consume(channel)
