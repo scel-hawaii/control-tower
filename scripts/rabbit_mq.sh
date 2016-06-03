8ef24a1eb31969f1c6145e7a00973e70767cc311
Kenny Luong <luong97@hawaii.edu>
2016-06-02 21:34:02 -0400

    Added script to run rabbitmq in a docker container

---
+++ scripts/rabbit_mq.sh
@@ -0,0 +1,9 @@
+# Commands to pull and run an instance of RabbitMQ via docker
+
+# Pull the rabbitmq docker file
+docker pull rabbitmq
+
+# Run the instance
+# Default port for the management GUI: 15672
+# Default port for the rabbitmq instance: 5627
+docker run -d --hostname my-rabbit --name some-rabbit -p 8081:15672 -p 8082:5627 rabbitmq:3-management
