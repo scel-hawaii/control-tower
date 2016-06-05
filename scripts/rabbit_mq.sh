4be42939625bf6b59e3f3ff71271ed5239d23b75
Kenny Luong <luong97@hawaii.edu>
2016-06-05 19:40:19 -0400

    Fixed port for rabbitmq

--- scripts/rabbit_mq.sh
+++ scripts/rabbit_mq.sh
@@ -6,4 +6,4 @@ docker pull rabbitmq
 # Run the instance
 # Default port for the management GUI: 15672
 # Default port for the rabbitmq instance: 5627
-docker run -d --hostname my-rabbit --name some-rabbit -p 8081:15672 -p 8082:5627 rabbitmq:3-management
+docker run -d --hostname my-rabbit --name some-rabbit -p 8081:15672 -p 8082:5672 rabbitmq:3-management
