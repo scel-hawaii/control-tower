c9c6588770c3cf6bd8e057871be5756b14340ae2
Kenny Luong <luong97@hawaii.edu>
2016-06-07 20:10:05 -0400

    Added echo statements to seed script

--- scripts/seed.sh
+++ scripts/seed.sh
@@ -1,6 +1,8 @@
 # Fetch seed data
+echo 'Fetching dataset...'
 wget http://static.scel-hawaii.org/data/outdoor_env.csv.zip
 unzip outdoor_env.csv.zip
+echo 'Done fetching dataset.'
 
 # Missing step: import the database schema
 
@@ -8,4 +10,7 @@ unzip outdoor_env.csv.zip
 # psql --host=localhost -p 8083 -U postgres -W secure_password -c "\\copy outdoor_env FROM 'outdoor_env.csv' CSV HEADER"
 
 # Import to machine db
+
+echo 'Importing dataset into database...'
 psql -c "\\copy outdoor_env FROM 'outdoor_env.csv' CSV HEADER"
+echo 'Done importing dataset.'
