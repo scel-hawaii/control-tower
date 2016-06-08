69f0e3d55ddbc5edbaaae97dff978dd0b5c3d537
Kenny Luong <luong97@hawaii.edu>
2016-06-07 20:08:02 -0400

    Changed default database import to machine instead of docker

--- scripts/seed.sh
+++ scripts/seed.sh
@@ -4,5 +4,8 @@ unzip outdoor_env.csv.zip
 
 # Missing step: import the database schema
 
-# Import
-psql --host=localhost -p 8083 -U postgres -W secure_password -c "\\copy outdoor_env FROM 'outdoor_env.csv' CSV HEADER"
+# Import to docker instance
+# psql --host=localhost -p 8083 -U postgres -W secure_password -c "\\copy outdoor_env FROM 'outdoor_env.csv' CSV HEADER"
+
+# Import to machine db
+psql -c "\\copy outdoor_env FROM 'outdoor_env.csv' CSV HEADER"
