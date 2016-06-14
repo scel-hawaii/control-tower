940bc591a932f349d56a12fedbf244e871d41f23
Kenny Luong <luong97@hawaii.edu>
2016-06-13 22:35:30 -0400

    Fixed db

--- api/battery.js
+++ api/battery.js
@@ -7,7 +7,7 @@ var _ = require('underscore')
  *
  */
 function pgQuery(qString, callback){
-	var connectionString = process.env.DATABASE_URL || 'pg://control_tower:password@localhost:5432/kluong';
+	var connectionString = process.env.DATABASE_URL || 'pg://control_tower:password@localhost:5432/control_tower';
 	pg.connect(connectionString, function(err, client, done) {
 	  if(err) {
 	    return console.error('error fetching client from pool', err);
