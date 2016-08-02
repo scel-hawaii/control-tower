9b77efb48b7a3220b3b278851a1550fe251b23ae
Kenny Luong <luong97@hawaii.edu>
2016-08-01 22:09:52 -0400

    Fix gateway errors

--- gateway.py
+++ gateway.py
@@ -7,15 +7,16 @@ import logging
 import sys
 import os
 import pickle
+import time
 
 class XBeeGateway:
 
-    def initialize(self):
+    def __init__(self):
         print "Setup"
         self.callbacks = []
 
     def register_callback(self, callback):
-        self.callbacks.push()
+        self.callbacks.append(callback)
 
     def setup(self):
         print "Init XBeeGateway"
@@ -30,23 +31,36 @@ class XBeeGateway:
             logging.warning(str(e))
             sys.exit(1)
 
+
     def begin(self):
         while True:
             xbee_frame = xbee.wait_read_frame()
-            data = {
-                    "timestamp": datetime.datetime.now(),
-                    "xbee_frame": xbee_frame
-                    }
-            d = pickle.dumps(data)
-
-            s = "[" + str(datetime.datetime.now()) + "] " + "Published xbee frame"
-            logging.info(s)
-
-            for callback in callbacks:
+            d = self.process_packet(xbee_frame)
+            for callback in self.callbacks:
                 callback(d)
 
+    def begin_fake(self):
+        xbee_frame = pickle.load( open( "save.p", "rb" ) )
+        while True:
+            time.sleep(1)
+            d = self.process_packet(xbee_frame)
+            for callback in self.callbacks:
+                callback(d)
+
+    def process_packet(self, xbee_frame):
+        data = {
+                "timestamp": datetime.datetime.now(),
+                "xbee_frame": xbee_frame
+               }
+        return pickle.dumps(data)
 
 if __name__ == "__main__":
+    def print_data(d):
+        print "Got data"
+        data = pickle.loads(d)
+        print data['timestamp']
+
     xbg = XBeeGateway()
-    xbg.setup()
-    xbg.begin()
+    xbg.register_callback(print_data)
+    # xbg.setup()
+    xbg.begin_fake()
