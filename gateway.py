7c7bdb26aa999d6bf0877f59ca6a107afb66f72d
Kenny Luong <luong97@hawaii.edu>
2016-08-01 20:15:36 -0400

    Removed rabbitmq code from XBee Gateway

--- gateway.py
+++ gateway.py
@@ -3,37 +3,50 @@
 from xbee import ZigBee
 import serial
 import datetime
-import pika
 import pickle
 import logging
+import sys
+import os
 
-logging.basicConfig(filename='gateway.log',level=logging.DEBUG)
+class XBeeGateway:
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-                   'localhost'))
-channel = connection.channel()
-channel.queue_declare(queue='hello')
+    def initialize(self):
+        print "Setup"
+        self.callbacks = []
 
-try:
-    ser_port = '/dev/ttyUSB0'
-    baud_rate = 9600
-    ser = serial.Serial(ser_port, baud_rate)
-    xbee = ZigBee(ser, escaped=True)
-except serial.serialutil.SerialException as e:
-    print "Serial Error: ", e
-    logging.warning("Serial error")
-    logging.warning(str(e))
-    sys.exit(1)
+    def register_callback(self, callback):
+        self.callbacks.push()
 
-while True:
-    xbee_frame = xbee.wait_read_frame()
-    data = {
-            "timestamp": datetime.datetime.now(),
-            "xbee_frame": xbee_frame
-            }
-    d = pickle.dumps(data)
+    def setup(self):
+        print "Init XBeeGateway"
+        ser_port = '/dev/ttyUSB0'
+        baud_rate = 9600
+        try:
+            ser = serial.Serial(ser_port, baud_rate)
+            xbee = ZigBee(ser, escaped=True)
+        except serial.serialutil.SerialException as e:
+            print "Serial Error: ", e
+            logging.warning("Serial error")
+            logging.warning(str(e))
+            sys.exit(1)
 
-    channel.basic_publish(exchange='',
-                          routing_key='hello',
-                          body=d)
-    logging.info("[" + str(datetime.datetime.now()) + "] " + "Published xbee frame")
+    def begin(self):
+        while True:
+            xbee_frame = xbee.wait_read_frame()
+            data = {
+                    "timestamp": datetime.datetime.now(),
+                    "xbee_frame": xbee_frame
+                    }
+            d = pickle.dumps(data)
+
+            s = "[" + str(datetime.datetime.now()) + "] " + "Published xbee frame"
+            logging.info(s)
+
+            for callback in callbacks:
+                callback(d)
+
+
+if __name__ == "__main__":
+    xbg = XBeeGateway()
+    xbg.setup()
+    xbg.begin()
