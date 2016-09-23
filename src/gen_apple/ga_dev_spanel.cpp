d2a8f83730c42df269bbd655e7bdfe5d81a6fbdb
Dylan Tokita <dtokita@hawaii.edu>
2016-09-22 16:40:32 -1000

    Spanel fix (#20)
    * Schema update for all generations and heartbeat packet.
    
    * Fix bug in ga_dev_spanel.cpp
    
    Changed digitalRead to analogRead

--- src/gen_apple/ga_dev_spanel.cpp
+++ src/gen_apple/ga_dev_spanel.cpp
@@ -8,7 +8,7 @@ int ga_dev_spanel_read(void){
     int value = 555;
 
     #ifndef SEN_STUB
-    value = digitalRead(_PIN_GA_SPANEL_);
+    value = analogRead(_PIN_GA_SPANEL_);
     #endif
 
     return value;
