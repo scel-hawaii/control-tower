3e248f1e2fae9d470a9c798df541219e4d9e1802
Andrew Obatake <aobatake@hawaii.edu>
2016-09-27 18:01:43 -1000

    created decoder class, implemented decoder class in packet tester

---
+++ decoder.py
@@ -0,0 +1,78 @@
+from xbee import ZigBee
+import sys
+import serial
+import datetime
+import struct
+import collections
+
+class Decoder:
+
+  def __init__(self):
+    self.schemaDict = {
+      'ga_legacy': 'HHBI'+'B'+'H'*6+'H'*6+'IhH'+'H'*20, 
+      '0': 'HHIH', #Heartbeat schema
+      '1': 'HHIHHIhHH', #Apple schema
+      '2': 'HHIHHHhHH', #Cranberry schema
+      '3': 'HHIHHIhHH'  #Dragonfruit Schema 
+    }
+
+  def check_schema(self, data):
+    self.schema = struct.unpack('<' + 'H', data[0:2])[0]
+    for key in self.schemaDict:
+      if str(self.schema) == key:
+        return True   
+    return False
+
+  def sort_packet(self, data):
+    fmt = '<' + self.schemaDict[str(self.schema)]
+    
+    dataDict = {}
+    unpacked_data = struct.unpack(fmt,data)
+
+    if self.schema == 1: #apple schema
+      dataDict["schema"] = unpacked_data[0]
+      dataDict["node_addr"] = unpacked_data[1]
+      dataDict["uptime_ms"] = unpacked_data[2]
+      dataDict["batt_mv"] = unpacked_data[3]
+      dataDict["panel_mv"] = unpacked_data[4]
+      dataDict["press_pa"] = unpacked_data[5]
+      dataDict["temp_c"] = unpacked_data[6]
+      dataDict["humidity_centi_pct"] = unpacked_data[7]
+      dataDict["apogee_w_m2"] = unpacked_data[8]
+
+    elif self.schema == 2: #cranberry schema
+      dataDict["schema"] = unpacked_data[0]
+      dataDict["node_addr"] = unpacked_data[1]
+      dataDict["uptime_ms"] = unpacked_data[2]
+      dataDict["batt_mv"] = unpacked_data[3]
+      dataDict["panel_mv"] = unpacked_data[4]
+      dataDict["apogee_w_m2"] = unpacked_data[5]
+      dataDict["temp_c"] = unpacked_data[6]
+      dataDict["humidity_centi_pct"] = unpacked_data[7]
+      dataDict["press_kpa"] = unpacked_data[8]
+    
+    elif self.schema == 3: #dragonfruit schema
+      dataDict["schema"] = unpacked_data[0]
+      dataDict["node_addr"] = unpacked_data[1]
+      dataDict["uptime_ms"] = unpacked_data[2]
+      dataDict["batt_mv"] = unpacked_data[3]
+      dataDict["panel_mv"] = unpacked_data[4]
+      dataDict["press_pa"] = unpacked_data[5] 
+      dataDict["temp_c"] = unpacked_data[6]
+      dataDict["humidity_centi_pct"] = unpacked_data[7]
+      dataDict["apogee_w_m2"] = unpacked_data[8]
+      
+    elif self.schema == 0: #heartbeat schema
+      dataDict["schema"] = unpacked_data[0]
+      dataDict["node_addr"] = unpacked_data[1]
+      dataDict["uptime_ms"] = unpacked_data[2]
+      dataDict["batt_mv"] = unpacked_data[3]
+
+    return dataDict 
+
+  def print_data(self, dataDict):
+    orderedData = collections.OrderedDict(sorted(dataDict.items()))
+    for key, value in orderedData.iteritems():
+      print key + ": " + str(value)
+
+
