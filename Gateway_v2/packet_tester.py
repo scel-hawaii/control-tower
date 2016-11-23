971f5968d2c6d9d583e1be73e548433045b9ca3e
Tim <byerst@hawaii.edu>
2016-11-22 16:46:26 -1000

    updated gateway to re-initialize script when no received packet in 30 sec

--- Gateway_v2/packet_tester.py
+++ Gateway_v2/packet_tester.py
@@ -4,6 +4,7 @@ import serial
 import datetime
 import struct
 import collections
+import threading
 
 from decoder import Decoder
 from xbee_gateway import XBeeGateway
@@ -20,5 +21,27 @@ xbg.register_callback(decoder.decode_data)
 port = raw_input("Please enter your serial port path/name: ")
 baud_rate = 9600
 
-xbg.setup_xbee(port, baud_rate)
-xbg.begin_test()
+t_flag = threading.Event()
+kill_flag = threading.Event()
+while True:
+	currentTime = datetime.datetime.now()
+	# log start times to file
+        fileName = 'reset_log.txt'
+	with open(fileName, 'a') as logfile:
+        	logfile.write(str(currentTime))
+		logfile.write('\n')
+
+	kill_flag.clear()
+	t_flag.set()
+	xbg.setup_xbee(port, baud_rate)
+	newThread = threading.Thread(target=xbg.begin_test, args=(1,t_flag,kill_flag))
+	newThread.daemon = True
+	newThread.start()
+
+	while t_flag.is_set():
+		print 'Flag Reset'
+		t_flag.clear()
+		#sleep 30 seconds then check to see if we have received anything
+		t_flag.wait(30)
+
+	kill_flag.set()
