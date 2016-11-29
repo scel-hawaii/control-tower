afd8cd9bf1c6fb808b2426cfaa9e7da41fea1fec
Dylan Tokita <dtokita@hawaii.edu>
2016-11-29 13:24:12 -1000

    Fix scaling factor for solar irradiance. (#59)
    * Fix scaling factor for solar irradiance.
    
    After discussion in meeting on Nov. 10th, 2016, we found that
    the scaling factor is incorrect. The scaling factor has been changed
    from 4.0/3.3 to 5.0/3.3.
    
    * Remove scaling factor from solar irradiance sensor.
    
    Decision made to send raw voltage instead of scaled up voltage
    to be consistient with generation Apple.

--- src/gen_cranberry/gc_dev_apogee_SP212.cpp
+++ src/gen_cranberry/gc_dev_apogee_SP212.cpp
@@ -10,7 +10,7 @@ uint16_t gc_dev_apogee_SP212_solar_irr_read(void){
     uint16_t value = 4000;
 
     #ifndef SEN_STUB
-    value = ((float)adc.readADC_SingleEnded(0)*188.0)/(1000.0)*(4.0/3.3);
+    value = ((float)adc.readADC_SingleEnded(0)*188.0)/(1000.0);
     #endif
 
     return value;
