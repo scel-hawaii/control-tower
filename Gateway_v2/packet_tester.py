29ce98967d4a3a9a12ab53057a2507126dc5256c
Tim <byerst@hawaii.edu>
2016-11-29 15:47:08 -1000

    add .txt to .gitignore and implement auto port detection when passing argument 'a'

--- Gateway_v2/packet_tester.py
+++ Gateway_v2/packet_tester.py
@@ -9,6 +9,8 @@ import threading
 from decoder import Decoder
 from xbee_gateway import XBeeGateway
 
+args = sys.argv
+
 decoder = Decoder()
 xbg = XBeeGateway()
 
@@ -17,12 +19,23 @@ decoder.register_callback(decoder.write_to_file)
 decoder.register_callback(decoder.write_to_db)
 xbg.register_callback(decoder.decode_data)
 
-# port can be accessed by /dev/serial/by-id/<device name> as opposed to /dev/tty/USB0. The latter will never change
-# use the line of code below when running simulation to manually enter port
-#port = raw_input("Please enter your serial port path/name: ")
+# if we have no special arguments
+if len(args) == 1:
+	# port can be accessed by /dev/serial/by-id/<device name> as opposed to /dev/tty/USB0. The latter will never change
+	# use the line of code below when running simulation to manually enter port
+	port = raw_input("Please enter your serial port path/name: ")
 
-# set port to usb FTDI Device
-port = '/dev/serial/by-id/usb-FTDI_FT231X_USB_UART_DN01DBGI-if00-port0'
+# if we want to automatically get the port...
+elif args[1] == 'auto' or args[1] == 'a':
+	print 'Automatically setting port for USB FTDI Device'
+	# set port to usb FTDI Device
+	port = '/dev/serial/by-id/usb-FTDI_FT231X_USB_UART_DN01DBGI-if00-port0'
+# we have no extra args 
+# we have confusing arguments
+else:
+	# quit the program
+	print 'Error, Invalid arguments'
+	sys.exit()
 
 baud_rate = 9600
 t_flag = threading.Event()
