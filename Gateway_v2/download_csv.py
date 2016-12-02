9ab9f67c5497a5f786edc3332c0d476546eb57f2
Tim <byerst@hawaii.edu>
2016-12-01 14:52:22 -1000

    add download_csv.py file to download formatted data to a csv file

---
+++ Gateway_v2/download_csv.py
@@ -0,0 +1,58 @@
+import psycopg2
+import sys
+import csv
+import os.path
+
+#some table name, must get table name from user
+table_name = "apple"
+
+filename = raw_input("Enter name of output file (include .csv extension):")
+# check if file already exists
+while(os.path.isfile(filename) == True):
+	filename = raw_input("That file already exists, enter a new filename") 	
+
+#make connection to db
+con = psycopg2.connect("dbname='control_tower' user='control_tower' password='password'")
+cur = con.cursor()
+
+# select the appropriate table
+cur.execute("SELECT * FROM %s;" %(table_name))
+table = cur.fetchall()
+# get names of columns
+colnames = [desc[0] for desc in cur.description]
+# add new columns for time/date to colnames
+colnames.remove("time_received")
+colnames.insert(0, "time")
+colnames.insert(0, "day")
+colnames.insert(0, "month")
+colnames.insert(0, "year")
+
+"""
+setup output csv file
+"""
+with open(filename, 'a') as csvfile:
+	headerString = ""
+	for value in colnames:
+		headerString += str(value) + ','
+	headerString = headerString[:-1]
+	headerString += '\n'
+	csvfile.write(headerString)
+
+	# write data to csv file
+	for line in table:
+		# clean up data ine
+		data = str(line)
+		data = data.replace("(", "")
+		data = data.replace(")", "")
+		data = data.replace(" ", "")
+		data = data.replace("datetime.datetime", "")
+		data = data.split(",")
+
+		dataString = ''
+    		for value in data:
+        		dataString += str(value)
+        		dataString += ','
+    			dataString = dataString[:-1]
+    			dataString += '\n'
+		csvfile.write(dataString)
+
