911c7522468a3540f0466bd58e576a0d17b5fed1
Kenny Luong <luong97@hawaii.edu>
2016-12-09 11:30:45 -1000

    Add hack to re-generate plots after deployment

--- data_grapher/deploy_remote.sh
+++ data_grapher/deploy_remote.sh
@@ -12,3 +12,11 @@ echo " DEPLOY_BRANCH=$DEPLOY_BRANCH " > deploy_env.sh
 scp deploy.sh $HOST:/tmp/
 scp deploy_env.sh $HOST:/tmp/
 ssh $HOST 'cd /tmp && bash deploy.sh'
+
+# Right now the deployment wipes away the old data - this
+# is a hack that re-generates the gnuplot based graphs after
+# a deployment.
+#
+# TODO: find out a better way to handle this later on.
+#
+ssh $HOST 'cd /home/control_tower/apps/data_grapher/data_grapher/ && bash plot_gnuplot.sh'
