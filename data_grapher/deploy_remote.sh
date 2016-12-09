3e29a2040f35d837030858a92fe44d0e1aa6e496
Kenny Luong <luong97@hawaii.edu>
2016-12-09 11:17:30 -1000

    Add initial support for remote deployments

--- data_grapher/deploy_remote.sh
+++ data_grapher/deploy_remote.sh
@@ -5,6 +5,10 @@
 # for this to correctly work make sure you have
 # scelserver-1 as an alias in your SSH config
 HOST=control_tower@scelserver-1
+DEPLOY_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+echo " DEPLOY_BRANCH=$DEPLOY_BRANCH " > deploy_env.sh
 
 scp deploy.sh $HOST:/tmp/
+scp deploy_env.sh $HOST:/tmp/
 ssh $HOST 'cd /tmp && bash deploy.sh'
