18f3c02110cbb0e653c7e4c712f37a32675ca1c8
Kenny Luong <luong97@hawaii.edu>
2018-03-20 20:57:04 -0500

    Add additionalc omment to gateway_server.py

--- gateway/src/gateway_server.py
+++ gateway/src/gateway_server.py
@@ -20,6 +20,12 @@ if len(args) == 1:
 	# port can be accessed by /dev/serial/by-id/<device name> as opposed to /dev/tty/USB0. The latter will never change
 	# use the line of code below when running simulation to manually enter port
 	# port = raw_input("Please enter your serial port path/name: ")
+        #
+        #
+        # Note from Kenny:
+        #   The best way I have to find out the device id currently is to go to the /dev/serial/by-id folder
+        #   and then see if a new device shows up after I plug in a device.
+        #
         port = "../test/ttyV2"
 
 # if we want to automatically get the port...
@@ -27,7 +33,7 @@ elif args[1] == 'auto' or args[1] == 'a':
 	print 'Automatically setting port for USB FTDI Device'
 	# set port to usb FTDI Device
 	port = '/dev/serial/by-id/usb-FTDI_FT231X_USB_UART_DN01DBGI-if00-port0'
-# we have no extra args 
+# we have no extra args
 # we have confusing arguments
 else:
 	# quit the program
