63ce31d74163b40ccaf6749a152f842efb99b370
Kenny Luong <luong97@hawaii.edu>
2018-04-11 21:32:10 -0500

    Add support for pubsub & cleanup stuff (#53)
    Pub-sub support should make it easier to build clients to observe incoming packets
    
    * Add initial publish-subscriber support
    * add new requirements
    * Clean up fake xbee code
    * Add changes

---
+++ gateway/src/decode.py
@@ -0,0 +1,34 @@
+import decoder
+import datetime
+import os
+import sys
+import json
+
+
+def print_packet(data):
+    print(json.dumps(data))
+    sys.stdout.flush()
+    # print(data['rf_data'])
+
+
+if sys.stdin.isatty():
+    rf_data = sys.argv[1]
+    d = decoder.Decoder()
+    d.register_callback(print_packet)
+    d.decode_data(rf_data.strip().decode('hex'), datetime.datetime.now())
+
+else:
+    # for line in sys.stdin:
+    for line in iter(sys.stdin.readline, ''):
+        d = decoder.Decoder()
+        d.register_callback(print_packet)
+        json_decode_error = False
+        try:
+            data_frame = json.loads(line)
+        except:
+            json_decode_error = True
+            print("There was an error trying to decode the json: " + str(line))
+
+        if json_decode_error == False:
+            d.decode_data(data_frame['rf_data'].strip().decode('hex'), datetime.datetime.now())
+
--- gateway/src/xbee_gateway.py
+++ gateway/src/xbee_gateway.py
@@ -9,6 +9,12 @@ import os
 import pickle
 import time
 import threading
+import sys
+import copy
+import uuid
+import json
+import zmq
+
 
 class XBeeGateway:
 
@@ -60,11 +66,60 @@ class XBeeGateway:
         Added by luong97@hawaii.edu on 2017-09-27
     """
     def begin_loop(self):
+        # socket.bind("tcp://*:%s" % port)
+        # port = "5556"
+        context = zmq.Context()
+        socket = context.socket(zmq.PUB)
+        socket.bind("ipc://tmp.socket")
+
+        topic = "testing"
+
         while(True):
             f = self.xbee.wait_read_frame()
+            """
+            now = datetime.datetime.now()
+            sys.stdout.write(str(now))
+            sys.stdout.write("  ")
+            sys.stdout.write(str(f))
+            sys.stdout.write("\n")
+            sys.stdout.flush()
+            """
+
+            uid = uuid.uuid4()
+
+            f_alt = copy.copy(f)
+            f_alt['source_addr_long'] = f_alt['source_addr_long'].encode('hex')
+            f_alt['source_addr'] = f_alt['source_addr'].encode('hex')
+            f_alt['rf_data'] = f_alt['rf_data'].encode('hex')
+            f_alt['options'] = f_alt['options'].encode('hex')
+            f_alt['packet_uuid'] = str(uid)
+            f_alt['time_recieved'] = str(datetime.datetime.now())
+
+            now = datetime.datetime.now()
+            sys.stdout.write(str(now))
+            sys.stdout.write("  ")
+            sys.stdout.write(json.dumps(f_alt))
+            sys.stdout.write("\n")
+            sys.stdout.flush()
+
+            # sys.stdout.write(str(f['source_addr_long']).encode('hex'))
+            # sys.stdout.write("\n")
+            # sys.stdout.flush()
+
             data = f['rf_data']
             timestamp = datetime.datetime.now()
+            socket.send(json.dumps(f_alt))
+            socket.send("COOL")
             for callback in self.callbacks:
                 callback(data, timestamp)
 
+
+if __name__ == "__main__":
+    xbee_gateway = XBeeGateway()
+    # serial_port = "/dev/serial/by-id/usb-FTDI_FT231X_USB_UART_DN00OK9M-if00-port0"
+    serial_port = "../test/ttyV2"
+    baud_rate = 9600
+
+    xbee_gateway.setup_xbee(serial_port, baud_rate)
+    xbee_gateway.begin_loop()
 
