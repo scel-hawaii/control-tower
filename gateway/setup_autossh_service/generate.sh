a0baff68cd4d727d0661593026108d40f181df38
Kenny Luong <luong97@hawaii.edu>
2018-04-13 20:45:23 -0500

    Add some scripts to help setup autossh

---
+++ gateway/setup_autossh_service/generate.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# Generates the system files
+#
+echo "Requesting sudo access..."
+sudo pwd
+
+MAC_ADDRESS=$(/sbin/ifconfig | grep Ethernet | awk '{print $5}')
+
+
+# Reverse SSH will take one port on the main system
+declare -A reverse_ssh_ports
+reverse_ssh_ports['b8:27:eb:8d:99:9c']=2222
+
+# reserved for other raspberry pi's
+# reverse_ssh_ports['b8:27:eb:8d:99:9c']=2223
+# reverse_ssh_ports['b8:27:eb:8d:99:9c']=2224
+
+# Iterate through the associate array
+for i in "${!reverse_ssh_ports[@]}"
+do
+    echo "key  : $i"
+    echo "value: ${reverse_ssh_ports[$i]}"
+done
+
+
+exit 1
+
+if [ "$MAC_ADDRESS" == "" ]; then
+    echo "ERROR: MAC address could not be detected"
+    exit 1
+fi
+
+echo "The MAC address is $MAC_ADDRESS"
+
+if [ "$MAC_ADDRESS" == "b8:27:eb:8d:99:9c" ]; then
+    echo "COOL"
+else
+    echo "ERROR you have an unregistered device."
+    exit 1
+fi
+
+exit 0
+
+BIND_PORT=1111
+cp base_files/* generated_files
+sed -i "s/{{LOCAL_BIND_PORT}}/$BIND_PORT/" generated_files/autossh-postgres
+
+
