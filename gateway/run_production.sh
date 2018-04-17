e9437c0b6c85182529dbb41050ac26f2d9d6fc07
Kenny Luong <luong97@hawaii.edu>
2018-04-16 15:57:45 -1000

    Add convenience scripts for python venv

--- gateway/run_production.sh
+++ gateway/run_production.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ ! -d "env" ]; then
+    echo "Setting up python virtual environment for the first time (this only needs to be done once)..."
+    echo "This may take a couple of minutes..."
+    ./setup_venv.sh
+fi
+
+source env/bin/activate
+
 PS_OUTPUT=$(pgrep gateway_server.py)
 
 if ! [ -z "$PS_OUTPUT" ]; then
