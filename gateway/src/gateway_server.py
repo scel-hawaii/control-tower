924639d8454249194c73534aa2944f0358d3a803
Kenny Luong <luong97@hawaii.edu>
2020-02-19 01:27:02 +0000

    Fix auto logic

--- gateway/src/gateway_server.py
+++ gateway/src/gateway_server.py
@@ -17,7 +17,6 @@ args = sys.argv
 
 port = ""
 # if we have no special arguments
-if len(args) == 2:
 	# port can be accessed by /dev/serial/by-id/<device name> as opposed to /dev/tty/USB0. The latter will never change
 	# use the line of code below when running simulation to manually enter port
 	# port = raw_input("Please enter your serial port path/name: ")
@@ -27,13 +26,15 @@ if len(args) == 2:
     #   The best way I have to find out the device id currently is to go to the /dev/serial/by-id folder
     #   and then see if a new device shows up after I plug in a device.
     #
-    port = sys.argv[1]
 
 # if we want to automatically get the port...
-elif args[1] == 'auto' or args[1] == 'a':
+if args[1] == 'auto' or args[1] == 'a':
 	print 'Automatically setting port for USB FTDI Device'
 	# set port to usb FTDI Device
 	port = '/dev/serial/by-id/usb-FTDI_FT231X_USB_UART_DN01DBGI-if00-port0'
+
+elif len(args) == 2:
+    port = sys.argv[1]
 
 # we have no extra args
 # we have confusing arguments
